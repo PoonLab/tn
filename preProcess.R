566d8c2ce0f2c3cfacc83485724752f652cac544
Art Poon <artpoon@gmail.com>
2018-10-11 15:44:02 -0400

    Update preProcess.R

--- preProcess.R
+++ preProcess.R
@@ -2,23 +2,33 @@
 library(seqinr)
 library(caret)
 
-createInput <- function(seq, newDir) 
-{
+createInput <- function(seq, newDir) {
+	# @param seq:  FIXME: What is this expected input?
+	# @param newDir:  
+	# @return:
+	
+	# TODO: write a check that dataframe <seq> has field <year>
+	
 	seqInput <- split(seq, f=seq$year)
 	dir.create(newDir)
 
 	for (yearSeq in seqInput) {
-		yearFile <- paste(newDir,"/", yearSeq[1,'year'], ".fas", sep="")
+		yearFile <- paste(newDir, "/", yearSeq[1, 'year'], ".fas", sep="")
 		write.fasta(sequences=as.list(yearSeq$sequence), names=yearSeq$id, file.out=yearFile, nbchar=60, open="w")
 	}		
 }
 
 args = commandArgs(trailingOnly=TRUE)
+
+# set stringsAsFactor to FALSE so they are handled as character objects
 seq <- read.csv(args[1], stringsAsFactor=F)
 
 set.seed(8266)
+
+#FIXME: implement the next line in native R
 index <- createDataPartition(seq$year, p=0.5, list=FALSE, times=1)
 
+# FIXME: comment needed
 train <- seq[index, ]
 test <- seq[-index, ]
 
