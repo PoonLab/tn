b176f46810782ff071bb3109eeef4777bd8bddc9
cchato <connorchato@gmail.com>
2019-08-26 17:13:30 -0400

    clmpAnalysis growth calc working

--- WIP/clmpAnalysis.R
+++ WIP/clmpAnalysis.R
@@ -3,18 +3,148 @@
 
 require(clmp)
 
-#Expecting an ML tree from an alignmnent of HIV sequence data
-#Expecting patient information in the format ID_Date
-args = commandArgs(trailingOnly = T)
-t <- read.tree(args[1])
+#Import Distance Data
+impTN93Dist <- function(iFile) {
+  #From the input file, a tn93 output file. This
+  idf <- read.csv(iFile, stringsAsFactors = F)
+  temp1 <- sapply(idf$ID1, function(x) (strsplit(x,'_')[[1]])[[1]])
+  temp3 <- sapply(idf$ID2, function(x) (strsplit(x,'_')[[1]])[[1]])
+  
+  #Create data frame of edges (ie. Vertex interactions)
+  el <- data.frame(ID1=as.character(temp1), ID2=as.character(temp3),
+                   Distance = as.numeric(idf$Distance), stringsAsFactors= F)
+  
+  return(el)
+}
+
+#Check the size to make sure the new year is big enough
+sizeCheck <- function(iT) {
+
+  maxT <- max(iT$Time)
+  maxTi <- which(iT$Time==maxT)
+  
+  while(length(maxTi)<63) {
+    iT <- drop.tip(iT, maxTi)
+    iT$Time <- iT$Time[-maxTi]
+    iT$ID <- iT$ID[-maxTi]
+    
+    maxT <- max(iT$Time)
+    maxTi <- which(iT$Time==maxT)
+  }
+  
+  return(iT)
+}
+
+
+#Cut the Newest Year
+cutTime <- function(iT) {
+  
+  iT <- sizeCheck(iT)
+  
+  maxT <- max(iT$Time)
+  maxTi <- which(iT$Time==maxT)
+  
+  iT <- drop.tip(iT, maxTi)
+  iT$Time <- iT$Time[-maxTi]
+  iT$ID <- iT$ID[-maxTi]
+  
+  iT <- sizeCheck(iT)
+  
+  return(iT)
+}
+
+#Import Tree Data and annotate with ID and Time
+impTree <-function(iFile){
+  #args = commandArgs(trailingOnly = T)
+  t <- read.tree(f)
+  
+  #Establish a set of node ids coupled with collection dates
+  temp <- sapply(t$tip.label, function(x) strsplit(x, '_')[[1]])
+  ids <- temp[1,]
+  times <- as.numeric(temp[2,])
+  
+  t$Time <- times
+  t$ID <- ids
+  
+  t <- sizeCheck(t)
+  
+  return(t)
+}
+
+#Import Data
+treeFile <- "~/Data/Seattle/analysis/FTStsubB.nwk"
+TN93File <- "~/Data/Seattle/tn93StsubB.txt" 
+
+Dist <- impTN93Dist(TN93File)
+nT <- impTree(treeFile)
+oT <- cutTime(nT)
+
+#Establish new and old clusters
+nRes <- clmp(nT, nrates = 2)
+oRes <- clmp(oT, nrates = 2)
+
+nC <- data.frame(ID=nT$ID, Time=nT$Time, Cluster=head(nRes$clusters, (length(nRes$clusters)+1)/2))
+oC <- data.frame(ID=oT$ID, Time=oT$Time, Cluster=head(oRes$clusters, (length(oRes$clusters)+1)/2))
+
+#Calculate growth (through closest membership)
+iC <- nC
+niC <- subset(iC, Time==max(iC$Time) & Cluster>0)
+closeNeighbs <- sapply(1:nrow(niC), function(i){
+  x <- niC[i,]
+  ioNeighb <- subset(iC, Cluster==x$Cluster & Time<max(iC$Time))
+  iDist <- subset(Dist, ID1%in%x$ID | ID2%in%x$ID)
+  iDist <- subset(iDist, ID1%in%ioNeighb$ID | ID2%in%ioNeighb$ID)
+  iDist$ID <- c(iDist$ID1,iDist$ID2)[c(iDist$ID1,iDist$ID2)%in%ioNeighb$ID]
+  iDist <- iDist[,c("Distance", "ID")]
+  
+  if(nrow(iDist)>0) {
+    iMin <- subset(iDist, Distance==min(Distance))[1,]$ID
+  } else {
+    iMin <- "Blah"
+  }
+  
+  return(iMin)
+})
+
+table(subset(oC, ID%in%closeNeighbs)$Cluster)
+
+
+
+
+
+
+
+
+
+
+######################Playing with nRates
+
+l <- lapply(1:10, function(nr){
+  ####- TO-DO: Modulate parameters of clump to produce different sets of cluster data
+  table(clusters$Cluster)
+  return(table(clusters$Cluster))
+})
+
+sings <- sapply(l, function(lsub) {unname(lsub["0"])})
+maxs <- sapply(l, function(lsub) {unname(max(tail(lsub,-1)))})
+means <-  sapply(l, function(lsub) {
+  cluA <- tail(lsub,-1)
+  cluB <- sum(cluA)-max(cluA)
+  cluB/(length(cluA)-1)
+})
+
+dfOld <- data.frame(sings,maxs,means)
 
 ####- TO-DO: Modulate parameters of clump to produce different sets of cluster data
 res <- clmp(t)
 
 #Establish a set of node ids coupled with collection dates
 temp <- sapply(res$tip.label, function(x) strsplit(x, '_')[[1]])
-nodes <- temp
-attr(nodes, "year") <- as.numeric(temp[2,])
+ids <- temp[1,]
+times <- as.numeric(temp[2,])
+
+clusters <- data.frame(ID=ids, Time=times, Cluster=head(res$clusters, (length(res$clusters)+1)/2))
+return(table(clusters$Cluster))
 
 #To make clmp cluster data comparable to tn93 cluster data
 ####- TO-DO: Pass to tn93 analysis software
