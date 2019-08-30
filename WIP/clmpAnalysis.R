c74a58172b776461090079d9f0a5205b10a2d460
Connor Chato <ConnorChato@gmail.com>
2019-08-30 10:41:30 -0400

    Obtaining GAICs

--- WIP/clmpAnalysis.R
+++ WIP/clmpAnalysis.R
@@ -70,23 +70,23 @@ impTree <-function(iFile){
   return(t)
 }
 
-simGrow <- function(iT, Dist=Dist) {
+simGrow <- function(iT, Dist=Dist, nrates=2) {
 
   nT <- iT
   oT <- cutTime(iT)
   
   #Establish new and old clusters
-  nRes <- clmp(nT, nrates = 2)
-  oRes <- clmp(oT, nrates = 2)
+  nRes <- clmp(nT, nrates)
+  oRes <- clmp(oT, nrates)
   
-  nC <- data.frame(ID=nT$ID, Time=nT$Time, Cluster=head(nRes$clusters, (length(nRes$clusters)+1)/2))
-  oC <- data.frame(ID=oT$ID, Time=oT$Time, Cluster=head(oRes$clusters, (length(oRes$clusters)+1)/2))
+  nC <- data.frame(ID=nRes$ID, Time=nRes$Time, Cluster=as.numeric(head(nRes$clusters, (length(nRes$clusters)+1)/2)), stringsAsFactors=F)
+  oC <- data.frame(ID=oRes$ID, Time=oRes$Time, Cluster=as.numeric(head(oRes$clusters, (length(oRes$clusters)+1)/2)), stringsAsFactors=F)
   
   oC[oC$Cluster==0,]$Cluster <- seq((max(oC$Cluster)+1), nrow(oC[oC$Cluster==0,])+(max(oC$Cluster)))
   
   #Calculate growth (through closest membership)
-  niC <- subset(nC, Time==max(nC$Time) & Cluster>0)
-  oiC <- subset(nC, Time<max(nC$Time) & Cluster>0)
+  niC <- subset(nC, Time==max(nC$Time))
+  oiC <- subset(nC, Time<max(nC$Time))
   closeNeighbs <- clsFilt(niC, oiC, Dist)
   
   growth <- table(oC$Cluster)
@@ -99,21 +99,23 @@ simGrow <- function(iT, Dist=Dist) {
 
 bpeFreq <- function(iClu, Dist=Dist) {
   
-  clu <- data.frame(ID=iClu$ID, Time=iClu$Time, Cluster=head(iClu$clusters, (length(iClu$clusters)+1)/2))
+  clu <- data.frame(ID=iClu$ID, Time=iClu$Time, Cluster=as.numeric(head(iClu$clusters, (length(iClu$clusters)+1)/2)), stringsAsFactors=F)
   times <- as.numeric(levels(as.factor(clu$Time)))
   
   eCounts <- bind_rows(lapply(tail(times,-1), function(x) {
-    nC <- subset(clu, Time==x & Cluster>0)
-    oC <- subset(clu, Time<x & Cluster>0)
-    nTot <- nrow(nC) 
-    clsIDs <- clsFilt(nC, oC, Dist)
-    
+
+    nC <- subset(clu, Time==x)
+    oC <- subset(clu, Time<x) 
+    nTot <- nrow(nC)
     tDiffs <- max(nC$Time) - as.numeric(levels(as.factor(oC$Time)))
+    clsIDs <- clsFilt(nC, oC, Dist)
     pos <- table(subset(oC, ID %in% clsIDs)$Time)
     totPos <- rep(0,length(tDiffs))
     totPos[max(nC$Time)-as.numeric(names(pos))] <- unname(pos)
     data.frame(Positive=totPos, Total=nTot, tDiff=tDiffs)
+    
   }))
+  
   
   return(eCounts)
 }
@@ -121,12 +123,15 @@ bpeFreq <- function(iClu, Dist=Dist) {
 #TO-DO: This is a Slow Function (thus slowing down eCounts and simGrow).
 clsFilt <- function(nC, oC, Dist=Dist) {
   
+  nC <- subset(nC, Cluster>0)
+  oC <- subset(oC, Cluster>0) 
+  
   #For every new case, find it's closest neighbour from the old cases
   closeNeighbs <- sapply(1:nrow(nC), function(i){
     x <- nC[i,]
     
     #The list of old neighbours to new case "x"
-    ioNeighb <- subset(oC, Cluster==x$Cluster & Time<max(iC$Time))
+    ioNeighb <- subset(oC, Cluster==x$Cluster & Time<max(nC$Time))
     
     #Obtain the distances of all old members of case x's cluster
     iDist <- subset(Dist, ID1%in%x$ID | ID2%in%x$ID)
@@ -144,87 +149,45 @@ clsFilt <- function(nC, oC, Dist=Dist) {
   })
   
   #Remove instances where a given new case has no close neighbours
-  closeNeighbs <-  
+  closeNeighbs <- closeNeighbs[!closeNeighbs%in%""]
   
   return(closeNeighbs)
 }
 
-clmpAnalysis <- function(iT, Dist=Dist) {
+clmpAnalysis <- function(iT, Dist=Dist, nrates=2) {
 
   oT <- cutTime(iT)
-  clu <- clmp(oT, nrates = 2)
+  clu <- clmp(oT, nrates)
   ageD <- bpeFreq(clu, Dist)
   
-  clu <- data.frame(ID=clu$ID, Time=clu$Time, Cluster=head(clu$clusters, (length(clu$clusters)+1)/2))
+  clu <- data.frame(ID=clu$ID, Time=clu$Time, Cluster=as.numeric(head(clu$clusters, (length(clu$clusters)+1)/2)), stringsAsFactors=F)
   
   #Obtain a model of case connection frequency to new cases as predicted by individual case age
   #Use this to weight cases by age
   mod <- glm(cbind(Positive, Total) ~ tDiff, data=ageD, family='binomial')
   clu$Weight <- predict(mod, data.frame(tDiff=max(iT$Time)-clu$Time), type='response')
   
-  growth <- simGrow(iT,Dist)
+  growth <- simGrow(iT,Dist, nrates)
   clu[clu$Cluster==0,]$Cluster <- seq((max(clu$Cluster)+1), nrow(clu[clu$Cluster==0,])+(max(clu$Cluster)))
   csize <- table(clu$Cluster)
-  
+
   #Create two data frames from two predictive models, one based on absolute size (NULL) and our date-informed model
-  df1 <- data.frame(Growth = growth, Pred = sapply(names(csize), function(x) { sum(subset(clu, Cluster==as.numeric(x))$Weight) }))
-  df2 <- data.frame(Growth = growth, Pred =  csize * (sum(growth)/sum(csize)))
+  df1 <- data.frame(Growth = as.numeric(growth), Pred = sapply(names(csize), function(x) { sum(subset(clu, Cluster==as.numeric(x))$Weight) }))
+  df2 <- data.frame(Growth = as.numeric(growth), Pred =  as.numeric(csize) * (sum(growth)/sum(csize)))
   fit1 <- glm(Growth ~ Pred, data = df1, family = "poisson")
   fit2 <- glm(Growth ~ Pred, data = df2, family = "poisson")
   
+  gaic <- fit2$aic - fit1$aic 
+  
+  return(gaic)
 }
 
+###### MAIN ######
 #Import Data
 TN93File <- "~/Data/Seattle/tn93StsubB.txt" 
 treeFile <- "~/Data/Seattle/analysis/FTStsubB.nwk"
+
 Dist <- impTN93Dist(TN93File)
 t <- impTree(treeFile)
 
-
-
-
-
-
-cc <- clmp(t,nrates = 2)
-
-
-
-saveRDS(res, file="BPE.rds")
-
-
-
-######################Playing with nRates
-
-l <- lapply(1:10, function(nr){
-  ####- TO-DO: Modulate parameters of clump to produce different sets of cluster data
-  table(clusters$Cluster)
-  return(table(clusters$Cluster))
-})
-
-sings <- sapply(l, function(lsub) {unname(lsub["0"])})
-maxs <- sapply(l, function(lsub) {unname(max(tail(lsub,-1)))})
-means <-  sapply(l, function(lsub) {
-  cluA <- tail(lsub,-1)
-  cluB <- sum(cluA)-max(cluA)
-  cluB/(length(cluA)-1)
-})
-
-dfOld <- data.frame(sings,maxs,means)
-
-####- TO-DO: Modulate parameters of clump to produce different sets of cluster data
-res <- clmp(t)
-
-#Establish a set of node ids coupled with collection dates
-temp <- sapply(res$tip.label, function(x) strsplit(x, '_')[[1]])
-ids <- temp[1,]
-times <- as.numeric(temp[2,])
-
-clusters <- data.frame(ID=ids, Time=times, Cluster=head(res$clusters, (length(res$clusters)+1)/2))
-return(table(clusters$Cluster))
-
-#To make clmp cluster data comparable to tn93 cluster data
-####- TO-DO: Pass to tn93 analysis software
-clu <- list()
-clu$membership <- res$clusters
-clu$csize <- unname(table(res$clusters)) #Cluster number 0 is reserved for all singletons 
-clu$no <- clu1$csize[[1]] + (length(clu1$csize) - 1)
+clmpAnalysis(t, Dist, nrates = 4)
