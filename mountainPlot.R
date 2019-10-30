a7c682a711147a6294ffc49725988cf07b14f4b5
cchato <connorchato@gmail.com>
2019-10-30 16:51:15 -0400

    subT Update

--- mountainPlot.R
+++ mountainPlot.R
@@ -7,9 +7,9 @@ gaics <- sapply(GD, function(x) {x$gaic})
 mod_bad <- sapply(GD_bad, function(x) {summary(x$ageFit)$aic})
 mod <- sapply(GD, function(x) {summary(x$ageFit)$aic})
 
+cutoffs <- seq(0, 0.04, 0.0008)
 
-
-plot(mod, ylab="GAIC is measured near the bottom)", xlab="Tuning", ylim=c(-80,400))
+plot(cutoffs, mod, ylab="GAIC is measured near the bottom)", xlab="Tuning", ylim=c(-80,400))
 
 bg <- par('usr')
 rect(xl=bg[1], yb=bg[3], xr=bg[2], yt=bg[4], col='blanchedalmond', border=NA)
@@ -22,16 +22,15 @@ abline(h=0)
 lines(mod_bad, lwd=5, lty=2, col="azure2")
 
 
-x <- 1:length(mod)
+x <- cutoffs
 y1 <- mod
 y2 <- mod_bad
 
 polygon(c(x,rev(x)), c(y1, rev(y2)) , col="darkslategray4")
-lines(mod-mod_bad, lwd=3, col="darkslategray4")
-points(mod-mod_bad, pch=21, bg="black", col="white")
+lines(cutoffs, mod-mod_bad, lwd=3, col="darkslategray4")
+points(cutoffs, mod-mod_bad, pch=21, bg="black", col="white")
 
-abline(v= which(abs(mod-mod_bad) == max(abs(mod-mod_bad))), lty=2, lwd=2)
-legend("topright", bg="white", cex=1.5,
-       legend=c(paste0("Optimum at: ", which(abs(mod-mod_bad) == max(abs(mod-mod_bad)))*0.0008*100, "%"), 
-                paste0("Min GAIC: ", round(min(mod-mod_bad))) ))
-       
+abline(v= which(abs(mod-mod_bad)== max(abs(mod-mod_bad)))*0.0008 , lty=2, lwd=2)
+text(x= which(abs(mod-mod_bad)==max(abs(mod-mod_bad)))*0.0008, y=min(mod-mod_bad), cex=1.2, adj=0, 
+     labels= c(paste0(round(min(mod-mod_bad)), " at ", which(abs(mod-mod_bad)==max(abs(mod-mod_bad)))*0.0008*100, "%"))) 
+     
---
+++ subT_An.R
@@ -0,0 +1,161 @@
+require("ape")
+require("phangorn")
+
+#A simple function, removing sequences that sit above a maximum time point (@param: maxT)
+tFilt <- function(iFile, maxT, oFile) {
+  
+  #Obtain IDs and times of Sequences
+  seqs <- read.dna(iFile, format = "fasta", as.character = T)
+  IDs <- names(seqs[,1])
+  times <- as.numeric(sapply(IDs, function(x) (strsplit(x,'_')[[1]])[[2]]))
+  keepInd <- which(times<maxT) 
+  
+  #Write only those sequences with ambiguity below 1.5% and sequence length above 85%
+  write.dna(seqs[keepInd,], oFile, "fasta")
+}
+
+#A function to create a temporary tree (currently uses FastTree)
+#TODO: Finalize with tempfile, raxmL and potential skipping (probably skipped at the imp level)
+makeTree <- function(iFile, MLfunLoc="~/Desktop/FastTree/FastTree",
+                     opt="-nt", relation=">", oFile="~/tree.nwk") {
+  
+  #Runtext is input
+  runtext <- paste(MLfunLoc, opt, iFile, relation, oFile)
+  system(runtext)
+  
+}
+
+#Import Tree Data and annotate with sequence ID and Time
+#Sequences must be dated with the date separated from the id by '_'. 
+##TO-DO: Currently only accepts year dates. Work to allow more specific dates. 
+impTree <-function(iFile){
+  #@param iFile: The name/path of the input file (expecting a newick file)
+  #@preturn: An ape tree object with associated lists of sequence ID and Time
+  
+  #Creating an ape tree object from the newick file
+  t <- read.tree(iFile)
+  
+  #Obtain lists of sequence ID and Time
+  temp <- sapply(t$tip.label, function(x) strsplit(x, '_')[[1]])
+  ids <- temp[1,]
+  times <- as.numeric(temp[2,])
+  
+  #Assign those lists to the tree
+  t$Time <- times
+  t$ID <- ids
+  
+  #Summarize internal branch length information
+  nodes <- unique(t$edge[,1])
+  dist <- sapply(nodes, function(x) {
+    if(x%in%t$edge[,2]){
+      t$edge.length[which(t$edge[,2]%in%x)]
+    }
+    else {NA}
+  })
+    
+  #A summary of cluster-relevant node information
+  t$nSum <- data.frame(NodeID=nodes, Dist=dist, Boot=as.numeric(oT$node.label))
+  
+  return(t)
+}
+
+##TO-DO: Fuse two filt functions?
+
+#A simple function, removing edges that sit above a maximum ancestral edge length (@param:maxD).
+dFilt <- function(iT, maxD) {
+  iT$nSum <- subset(iT$nSum, Dist<maxD)
+  return(iT)
+}
+
+#A simple function, removing edges that sit under a minimum bootstrap (@param:maxD).
+bFilt <- function(iT, maxB) {
+  iT$nSum <- subset(iT$nSum, Dist<maxD)
+  return(iT)
+}
+
+#Create clusters based on component clustering by some measure of genetic distance
+STClu <- function(iT) {
+  #@param iG: The inputted graph. Expecting all vertices, but some edges filtered by distance.
+  #@return: The inputted graph, annotated with a cluster size summary and case membership in the vertices section
+
+  #Obtain the descendants of each node
+  decs <- lapply(nodes, function(x){
+    l <- Descendants(iT,x,"all")
+    l <- setdiff(l, 1:length(iT$ID))
+    return(l)
+  })
+  
+  #Obtain only decsendant lists who are all within the list of clusters
+  clu <- decs[sapply(decs, function(x){
+    (length(x)==length(which(x%in%iT$dSum$NodeID)))&&length(x>1)
+  })]
+  
+  #Collapse all vectors that are subsets of other vectors in list
+  clu <- clu[!sapply(seq_along(clu), function(i) max(sapply(clu[-i],function(L) all(clu[[i]] %in% L))))]
+  
+  clu <- lapply(clu, function(x) {
+    chd <- iT$edge[which(iT$edge[,1]%in%x),2]
+    chd <- chd[chd%in%1:length(iT$ID)]
+  })
+  
+  return(clu)
+}
+
+
+###############Testing
+
+
+#Make Option
+makeT <- F
+if(makeT) {
+  
+  #TreeFile Tests (FastTree)
+  makeTree("~/Data/Seattle/SeattleB_PRO.fas", oFile="~/nT.nwk")
+  #tf <- tempfile("nT", fileext=".nwk")
+  #makeTree("~/Data/Seattle/SeattleB_PRO.fas", oFile=tf)
+  
+  nT <- impTree("~/nT.nwk")
+  maxT <- max(nT$Time)-1 #TO-DO: Use Table for this
+  #tf1 <- tempfile("oT", fileext=".fas")
+  tFilt("~/Data/Seattle/SeattleB_PRO.fas", maxT, "~/oT.fas")
+  
+  
+  makeTree("oT.fas", oFile="~/oT.nwk")
+  #tf2 <- tempfile("oT", fileext=".nwk")
+  #makeTree(tf1, oFile=tf2)
+}
+
+#Import trees from files
+oT <- impTree("oT.nwk")
+nT <- impTree("nT.nwk")
+
+#Plot Test
+plotT <- F
+if(plotT){
+  
+  #Clustering Test
+  cutoffs <- seq(0.001,0.02,0.0005)
+  res <- lapply(cutoffs, function(x){
+    subT <- dFilt(oT, x)
+    c <- STclu(subT)
+    return(c)
+  })
+  
+  cnum <- sapply(res, function(c) {
+    clustered <- sapply(c, function(x){length(x)})
+    sing <- rep(1,length(t$ID)-sum(clustered))
+    cnum <- (length(clustered)+length(sing))
+    return(cnum)
+  })
+  
+  csize <- sapply(res, function(c) {
+    clustered <- sapply(c, function(x){length(x)})
+    sing <- rep(1,length(t$ID)-sum(clustered))
+    csize <- mean(c(sing,clustered))
+    return(csize)
+  })
+  
+  par(mfrow=c(1,2))
+  plot(cutoffs, cnum, xlab="Threshold", ylab="Number of Clusters")
+  plot(cutoffs, csize, xlab="Threshold", ylab="Mean Cluster Size")
+}
