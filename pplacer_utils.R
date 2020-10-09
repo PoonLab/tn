334e8a30d566510219c0639f9f22474eff229880
cchato <connorchato@gmail.com>
2020-10-09 11:46:42 -0400

    Utils Update

--- pplacer_utils.R
+++ pplacer_utils.R
@@ -1,5 +1,6 @@
 require(ape)
 require(rjson)
+require(digest)
 
 #Obtain n samples from a given fasta file, save this to a directory
 sampleFasta <- function(iFile, n=100){
@@ -16,12 +17,14 @@ sampleFasta <- function(iFile, n=100){
 }
 
 #Run FastTree on a set of sequences generated through sample fasta
-multiRun <- function(sampsDir, n) {
+multiRun <- function(sampsDir) {
+  
+  n <- length(list.files(sampsDir, pattern = "\\.fasta$"))
   
   for(i in 1:n) {
     system(paste0("FastTree -gtr -log ", sampsDir, "/log", i, ".txt ",
-           "-nt ", sampsDir, "/samp", i, ".fasta ",
-           "> ", sampsDir, "/tree", i, ".nwk"))
+                  "-nt ", sampsDir, "/samp", i, ".fasta ",
+                  "> ", sampsDir, "/tree", i, ".nwk"))
   }
 }
 
@@ -56,27 +59,91 @@ getNS <- function(iFile, oFile, new){
 }
 
 #A similar function to the taxit create function found in pplacer
-taxitCreate <- function(samp, tree, json, stats, log, oDir, nSeqs, short="") {
+taxitCreate <- function(treeF, logF, statsF, locus="pol_SubB", refpkg) {
   
-  #File Manipulation and directory setup 
-  refpkg <- paste0(oDir, "/", short, ".refpkg")
-  dir.create(refpkg)
+  #Copy files and update filenames to trim complete paths
+  file.copy(logF, refpkg)
+  file.copy(treeF, refpkg)
+  file.copy(statsF, refpkg)
+  logF <- tail(strsplit(logF, "/")[[1]],1)
+  treeF <- tail(strsplit(treeF, "/")[[1]],1)
+  statsF <- tail(strsplit(statsF, "/")[[1]],1)
   
-  #add components together
-  file.copy(json, refpkg)
-  file.copy(log, refpkg)
-  file.copy(tree, refpkg)
+  #Create contents JSON file
+  conF <- file(paste0(refpkg, "/CONTENTS.json"))
+  conText <- toJSON(list(
+    "files" = list(
+      "aln_fasta" = "fullAln.fasta" ,
+      "phylo_model" = statsF,
+      "tree" = treeF,
+      "tree_stats" = logF
+    ),
+    "rollback"=NULL,
+    "log"= c("Stripped refpkg (removed 0 files)",
+             "Loaded initial files into empty refpkg"),
+    "metadata" = list(
+      "create_date" = as.character(Sys.Date()),
+      "format_version" = "1.1",
+      "locus" = locus 
+    ),
+    "rollforward"=NULL,
+    "md5"= list(
+      "aln_fasta" = digest("fullAln.fasta", algo = "md5"),
+      "phylo_model" = digest(logF, algo = "md5"),
+      "tree" = digest(treeF, algo = "md5"),
+      "tree_stats" = digest(logF, algo = "md5")
+    )
+  ), indent=4)
+  write(conText, conF)
+  close(conF)
+}
+
+#A function to call taxit create many times on a directory of repeated runs
+multiTaxit <- function(sampsDir, n, short="", nSeqs, oDir) {
   
-  seqs <- c(read.FASTA(samp), read.FASTA(nSeqs))
-  write.FASTA(seqs, paste0(refpkgN, "/fullSeqs.fasta"))
+  oDir <- paste0(oDir, "/", short, "refpackages")
+  dir.create(oDir)
   
-  conFile <- file(paste0(refpkg, "/CONTENTS.json"))
-  writeLines(paste0(
-    "{\n
-    \t\"files\": {\n
-    \t\t\"aln_fasta\": \"",  
+  #
+  seqs <- lapply(1:n, function(i) {
+    samp <- paste0(sampsDir, "/", short, "samp", i, ".fasta")
+    seqs <- c(read.FASTA(samp), read.FASTA(nSeqs))
+    return(seqs)
+  })
+  
+  for (i in 1:n) {
+    #File Manipulation and directory setup 
+    refpkg <- paste0(oDir, "/", short, i, ".refpkg")
+    dir.create(refpkg)
     
-    "))
+    write.FASTA(seqs[[i]], paste0(refpkg, "/fullAlign.fasta"))
+    
+    treeF <- paste0(sampsDir, "/", short, "tree", i, ".nwk")
+    logF <- paste0(sampsDir, "/", short, "log", i, ".txt")
+    statsF <- paste0(sampsDir, "/", short, "stats", i, ".json")
+    
+    taxitCreate(treeF=treeF, logF=logF, statsF=statsF, refpkg = refpkg)
+  }
+}
 
+#Runs pplacer on all files in a directory to obtain placement files
+multiPplacer <- function(refDir, oDir) {
+  fs <- list.files(refDir)
+  
+  for (i in 1:length(fs)) {
+    f <- paste0(refDir, "/", fs[[i]])
+    system(paste0("pplacer -c ", f, " -o ", oDir, "/jplaceFile", i, " ", f, "/fullAlign.fasta"))
+    
+  }
+}
 
+#Runs guppy on all files in a directory to obtain growth files (inputs for tree growth method)
+multiGuppy <- function(refDir, oDir) {
+  fs <- list.files(refDir)
+  
+  for (i in 1:length(fs)) {
+    f <- paste0(refDir, "/", fs[[i]])
+    system(paste0("guppy sing --point-mass ", f, " -o ", oDir, "/growthFile", i, ".tree"))
+    
+  }
 }
--- translator.py
+++ translator.py
@@ -1,6 +1,7 @@
 import io 
 import re
 import argparse
+import json
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -11,6 +12,27 @@ def parse_args():
 
     parser.add_argument(
         '-o', type=str, default=(),    
+    )
+    
+    parser.add_argument(
+        '-datatype', type=str, default="DNA",    
+    )
+    
+    parser.add_argument(
+        '-subs_model', type=str, default="GTR",    
+    )
+
+    parser.add_argument(
+        '-program', type=str, default="FastTree version 2.1.10 Double precision (NoSSE3)",    
+    )
+
+    parser.add_argument(
+        '-ras_model', type=str, default="gamma",    
+    )
+
+
+    parser.add_argument(
+        '-alpha', type=str, default="1.0",    
     )
 
     return parser.parse_args()
@@ -25,22 +47,24 @@ if __name__ == '__main__':
     for line in iFile:
         if re.search(pattern, line):
             s = line.split()
-            oFile.write("{")
-            oFile.write("\n\t\"empirical_frequencies\": true,")
-            oFile.write("\n\t\"datatype\": \"DNA\", ")
-            oFile.write("\n\t\"subs_model\": \"GTR\",")
-            oFile.write("\n\t\"program\": \"FastTree version 2.1.10 Double precision (NoSSE3)\",")
-            oFile.write("\n\t\"ras_model\": \"gamma\",")
-            oFile.write("\n\t\"gamma\": {")
-            oFile.write("\n\t\t\"alpha\": 1.0,")
-            oFile.write("\n\t\t\"n_cats\": 20")
-            oFile.write("\n\t},")
-            oFile.write("\n\t\"subs_rates\": {")
-            oFile.write("\n\t\t\"ac\": {},".format(s[1]))
-            oFile.write("\n\t\t\"gt\": {},".format(s[2]))
-            oFile.write("\n\t\t\"at\": {},".format(s[3]))
-            oFile.write("\n\t\t\"ag\": {},".format(s[4]))
-            oFile.write("\n\t\t\"cg\": {},".format(s[5]))
-            oFile.write("\n\t\t\"ct\": {}".format(s[6]))
-            oFile.write("\n\t}")
-            oFile.write("\n}")
+            
+            oText = {}
+            oText["empirical_frequencies"] = "True"
+            oText["datatype"] = args.datatype
+            oText["subs_model"] = args.subs_model
+            oText["program"] = args.program
+            oText["ras_model"] = args.ras_model
+            gamma = {}
+            gamma["alpha"] = "1.0"
+            gamma["n_cats"] = "20"
+            oText["gamma"] = gamma
+            sub_rates = {}
+            sub_rates["ac"] = s[1]
+            sub_rates["gt"] = s[2]
+            sub_rates["at"] = s[3]
+            sub_rates["ag"] = s[4]
+            sub_rates["cg"] = s[5]
+            sub_rates["ct"] = s[6]
+            oText["sub_rates"] = sub_rates
+
+            json.dump(oText, oFile, indent=4)
